0e7a0fc7d37ce93658d4a0962641d506e213ac9e
Huy Nguyen <huy.nguyen1219@gmail.com>
2017-12-26 21:59:53 -0800

    Second commit

---
+++ automation/theCollatzSequence.py
@@ -0,0 +1,17 @@
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result 
+
+try:
+
+    number = int(input("Give me a number:"))
+    while number != 1: 
+        number = collatz(number)
+except ValueError:
+    print('Type an interger')
