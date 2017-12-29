547a460addecc425621f9c080e97b66859b28428
Huy Nguyen <huy.nguyen1219@gmail.com>
2017-12-28 22:17:50 -0800

    changed

---
+++ Scripting/age.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+name = raw_input("What is your name? ")
+birthdate = raw_input("What is your birthdate")
+age = input("how old are you?")
+print("%s was born on %s" % (name,birthdate))
+print("Half of your age is %s" % (age/2))
