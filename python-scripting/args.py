668901e5fd781e00edbbf3fcca8096a9405d9a89
huytn12 <huy.nguyen1219@gmail.com>
2018-07-17 23:32:15 -0700

    script added

--- python-scripting/args.py
+++ python-scripting/args.py
@@ -3,6 +3,5 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename', help='the file to read')
+parser.add_argument("filename", help="the file to read")
 args = parser.parse_args()
-
